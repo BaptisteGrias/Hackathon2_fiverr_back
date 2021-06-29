58580739e7f6e336f13c0973196c7ab78f212e8d
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-29 13:56:16 +0200

    le gros commit

--- models/messageModel.js
+++ models/messageModel.js
@@ -2,11 +2,34 @@ const connection = require('../../db_config');
 
 const db = connection.promise();
 
-
+const create = ({
+    title, 
+    text,
+    iduser_sender,
+    iduser_dest
+}) => {
+    return db
+        .query('INSERT INTO message (title, text, iduser_sender, iduser_dest) VALUES (?, ?, ?, ?)', {
+            title, 
+            text,
+            iduser_sender,
+            iduser_dest
+        })
+        .then(([result]) => {
+            const messageId = result.insertId;
+            return { messageId, title, text, iduser_sender, iduser_dest };
+        });
+};
 
 const getMessageByUserId = (id) => {
+
+    const sql = `SELECT * from user
+    JOIN message ON iduser = iduser_sender
+    JOIN user ON iduser_dest = iduser
+    ORDER BY date`;
+
     return db
-        .query('SELECT * FROM message WHERE iduser_sender = ?', [id])
+        .query(sql, [id])
         .then(([results]) => results)
 }
 
