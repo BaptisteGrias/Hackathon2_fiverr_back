e62eb6cc5be52b351dd0e85c2a13fa037465ea57
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-06-29 14:29:22 +0200

    config error fixed

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const app = require('./app');
-const connection = require('./Config');
-const { setupRoutes } = require('./routes/router');
+const app = require("./app");
+const connection = require("./db_config");
+const { setupRoutes } = require("./routes/router");
 
 const PORT = process.env.PORT || 8000;
 
 setupRoutes(app);
 
 connection.connect(function (err) {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
+	if (err) {
+		console.error(`error connecting: ${err.stack}`);
+		return;
+	}
 
-  console.log(`connected as id ${connection.threadId}`);
+	console.log(`connected as id ${connection.threadId}`);
 });
 
 app.listen(PORT, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Express server listening on ${PORT}`);
-  }
+	if (err) {
+		console.error(err);
+	} else {
+		console.log(`Express server listening on ${PORT}`);
+	}
 });
