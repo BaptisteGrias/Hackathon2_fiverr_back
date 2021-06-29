3d79351debae5661ef2e627bc4ccafaeb2586ce1
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-06-29 15:35:52 +0200

    routes files edited, errors fixed

--- routes/fiverrMeet.js
+++ routes/fiverrMeet.js
@@ -1,85 +1,89 @@
-const fiverrMeetRouter = require('express').Router();
+const fiverrMeetRouter = require("express").Router();
 const fiverrMeet = require("../models/fiverrMeetModel");
 
+fiverrMeetRouter.get("/", (req, res) => {
+	const sqlFilters = [];
 
-fiverrMeetRouter.get('/', (req, res) => {
-
-    const sqlFilters = [];
-
-    if(req.query.region){
-        getFiverrMeetByRegion(req.query.region)
-        .then((results) => {
-            res.status(200).json(results)
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send('Error fetch all fiverrmeets')
-        })
-    } else {
-        user.getAllFiverrMeet()
-        .then((results) => {
-            res.status(200).json(results)
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send('Error fetch all fiverr meets')
-        })        
-    }
+	if (req.query && req.query.region) {
+		getFiverrMeetByRegion(req.query.region)
+			.then((results) => {
+				res.status(200).json(results);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(500).send("Error fetch all fiverrmeets");
+			});
+	} else {
+		fiverrMeet
+			.getAllFiverrMeet()
+			.then((results) => {
+				res.status(200).json(results);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(500).send("Error fetch all fiverr meets");
+			});
+	}
 });
 
-fiverrMeetRouter.get('/:id', (req, res) => {
-    message.getFiverrMeetById(req.params.id)
-        .then((results) => {
-            res.status(200).json(results);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send('Error fetch fiverr meet')
-        })
+fiverrMeetRouter.get("/:id", (req, res) => {
+	fiverrMeet
+		.getFiverrMeetById(req.params.id)
+		.then((results) => {
+			res.status(200).json(results);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).send("Error fetch fiverr meet");
+		});
 });
 
-fiverrMeetRouter.get('user/:id', (req, res) => {
-    message.getFiverrMeetByUserId(req.params.id)
-        .then((results) => {
-            res.status(200).json(results);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send('Error fetch fiverr meet')
-        })
+fiverrMeetRouter.get("user/:id", (req, res) => {
+	fiverrMeet
+		.getFiverrMeetByUserId(req.params.id)
+		.then((results) => {
+			res.status(200).json(results);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).send("Error fetch fiverr meet");
+		});
 });
 
-fiverrMeetRouter.get('/:id/user/:iduser', (req, res) => {
-    message.getOneFiverrMeetByUserId(req.params.id, req.params.iduser)
-        .then((results) => {
-            res.status(200).json(results);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send('Error fetch fiverr meet')
-        })
+fiverrMeetRouter.get("/:id/user/:iduser", (req, res) => {
+	fiverrMeet
+		.getOneFiverrMeetByUserId(req.params.id, req.params.iduser)
+		.then((results) => {
+			res.status(200).json(results);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).send("Error fetch fiverr meet");
+		});
 });
 
-fiverrMeetRouter.post('/', (req, res) => {
-    fiverrMeet.create(req.body)
-        .then((createdfiverrMeet) => {
-            res.status(201).json(createdfiverrMeet);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error saving the fiverr meet');
-        });
+fiverrMeetRouter.post("/", (req, res) => {
+	fiverrMeet
+		.create(req.body)
+		.then((createdfiverrMeet) => {
+			res.status(201).json(createdfiverrMeet);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send("Error saving the fiverr meet");
+		});
 });
 
-fiverrMeetRouter.put('/:id', (req, res) => {
-    User.update(req.params.id, req.body)
-        .then(() => {
-            res.status(200).json({ ...req.body });
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send('Error updating a fiverr meet');
-        });
+fiverrMeetRouter.put("/:id", (req, res) => {
+	fiverrMeet
+		.update(req.params.id, req.body)
+		.then(() => {
+			res.status(200).json({ ...req.body });
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(500).send("Error updating a fiverr meet");
+		});
 });
 
 module.exports = fiverrMeetRouter;
